e45441827822ffccd0ad37da90f31c6bd595f566
suryanashwin4u <suryanashwin4u@gmail.com>
2021-12-01 17:45:48 +0530

    mongo

---
+++ ajax_notes.js
@@ -0,0 +1,14 @@
+$.ajax({
+    type: "POST",
+    url: /filename,                                 //url
+    data: {
+        "key": value,                               //json data
+    },
+    dataType: "json",                           
+    success: function (obj) {                       //success function         
+        
+    },
+    error: function (errors) {                      //error function
+
+    }
+}); 
